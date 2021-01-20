2ebea9e49420bbe5f86e7a9ab0deac09451a9ef2
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-01-20 18:35:30 +0000

    First Commit
    Upload Basic parse Json Project

--- Basic JSON/ViewController.swift
+++ Basic JSON/ViewController.swift
@@ -13,7 +13,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func parseJson() {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        let result: Result?
+        do {
+            let jsonData = try Data(contentsOf: url)
+            result = try JSONDecoder().decode(Result.self, from: jsonData)
+            
+            if let result = result {
+                print(result)
+            } else {
+                print("Failed to parse")
+            }
+            return
+        } catch {
+    print("Error \(error)")
+        }
+    }
 }
 
+
+struct Result: Codable {
+    let data: [ResultItem]
+}
+
+struct ResultItem: Codable {
+    let title: String
+    let items: [String]
+}
